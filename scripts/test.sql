6f47c9110f6f1de0b2ae2be32b97b97e6479707c
Khiem Ton <29520244+th4tkh13m@users.noreply.github.com>
2022-03-10 00:59:26 +0000

    Create test.sql

---
+++ scripts/test.sql
@@ -0,0 +1,18 @@
+INSERT INTO AREA VALUES
+(1, 'Test')
+
+INSERT INTO ROOM VALUES
+(12345678, 1)
+
+
+INSERT INTO STUDENT VALUES
+(
+    12345678,
+    12345678,
+    'Test Student',
+    'M',
+    '2000-01-01',
+    '0912345678',
+    'Test Address',
+    '1234567890'
+)
