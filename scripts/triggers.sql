3b3631982d569761bb7f383a713bf32053a6fdfc
Khiem Ton <th4tkh13m@protonmail.com>
2022-03-11 17:14:19 +0700

    feat: trigger on room and facility

--- scripts/triggers.sql
+++ scripts/triggers.sql
@@ -15,3 +15,42 @@ BEGIN
         ROLLBACK TRANSACTION;
     END
 END
+GO
+
+CREATE OR ALTER TRIGGER limitFacility ON OWN
+AFTER INSERT, UPDATE
+AS
+BEGIN
+    DECLARE @count INT;
+    DECLARE @facCode NUMERIC(3);
+    SELECT @facCode = facCode
+    FROM inserted;
+    SELECT @count = COUNT(*)
+    FROM OWN
+    WHERE facCode = @facCode;
+    IF (@count > 1)
+    BEGIN
+        PRINT('Each student own only one facility');
+        ROLLBACK TRANSACTION;
+    END
+END
+GO
+
+
+CREATE OR ALTER TRIGGER roomLimit ON ROOM
+AFTER INSERT, UPDATE
+AS
+BEGIN
+    DECLARE @count INT;
+    DECLARE @areaCode NVARCHAR(4);
+    SELECT @areaCode = areaCode
+    FROM inserted;
+    SELECT @count = COUNT(*)
+    FROM ROOM
+    WHERE areaCode = @areaCode;
+    IF (@count > 30)
+    BEGIN
+        PRINT('Area is full');
+        ROLLBACK TRANSACTION;
+    END
+END
