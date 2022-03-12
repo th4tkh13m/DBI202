3c3adeba16ebcbd28390b176af253f093553e434
Khiem Ton <th4tkh13m@protonmail.com>
2022-03-12 15:13:24 +0700

    fix: 1 type of facility per student

--- scripts/triggers.sql
+++ scripts/triggers.sql
@@ -22,12 +22,9 @@ AFTER INSERT, UPDATE
 AS
 BEGIN
     DECLARE @count INT;
-    DECLARE @facCode NUMERIC(3);
-    SELECT @facCode = facCode
-    FROM inserted;
-    SELECT @count = COUNT(*)
-    FROM OWN
-    WHERE facCode = @facCode;
+    SELECT TOP 1 @count = COUNT(*)
+    FROM inserted
+    GROUP BY stuCode, facCode
     IF (@count > 1)
     BEGIN
         PRINT('Each student own only one facility');
